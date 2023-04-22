a4f9f564ba7e864f6562d113962af9df67c70e5f
unknown <rahularora993676@gmail.com>
2023-04-22 12:41:10 -0700

    done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
     <div style={{backgroundRepeat:"no-repeat",backgroundSize:"cover",backgroundImage:"url(https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2017/02/Photoshop-Replace-Background-Featured.jpg)",height:"700px"}} className="App">
       <Navbar/>
       <h1 style={{fontSize:"50px",color:"black"}}>Home</h1>
+      <footer style={{
+        marginTop:"450px",
+        fontSize:"30px",
+        color:"white",
+        fontWeight:"600"
+      }}>Made by 🧡Rahul Arora 🧡</footer>
     </div>
   );
 }
